25e0dd3c95d5dd5f19edeae884644fe4f18d6caa
hang.tian <hang.tian@cisdi.com.cn>
2023-10-25 10:47:47 +0800

    for test

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const SITE = {
   title: '潮流周刊',
-  author: 'Tw93',
-  description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
-  keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
+  author: 'Flicker',
+  description: '记录工程师 Flicker 的不枯燥生活，每周一发布，欢迎关注',
+  keywords: 'Weekly,Blog,Java，Python，Golang,Geek',
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
-  homePage: 'https://weekly.tw93.fun',
-  blogPage: 'https://tw93.fun/',
-  twitterId: 'HiTw93',
-  githubId: 'tw93',
-  repo: 'tw93/weekly'
+  homePage: 'https://weekly.flickerhub.com',
+  blogPage: 'https://flickerhub.com/',
+  twitterId: 'tianhang12',
+  githubId: 'augustrush8',
+  repo: 'augustrush8/weekly'
 };
 
